5c3ec95e23e933d082053fbd177cface20fd61e3
Zabraham-fab <mf1000gol@gmail.com>
2023-03-02 10:50:41 +0000

    several files fixed

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,6 +1,6 @@
 PATH="$PATH:/usr/local/bin:$HOME/bin"
 APP_NAME="petclinic"
-APP_REPO_NAME="clarusway-repo/petclinic-app-qa"
+APP_REPO_NAME="zabraham-repo/petclinic-app-qa"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 export AWS_REGION="us-east-1"
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
